fbc455f8fc5909509766a8568f0957555b924708
Brett <brett.g.hays.coding@gmail.com>
2017-12-13 19:20:07 -0700

    Through step 3

--- src/App.js
+++ src/App.js
@@ -8,15 +8,44 @@ import FamilyChanger from './components/FamilyChanger';
 import TextContainer from './components/TextContainer';
 
 class App extends Component {
-  // constructor
+  constructor(){
+    super();
+    this.state={
+      fontColor: 'black',
+      fontSize: 12,
+      fontFamily: 'monospace',
+      allowEdit: 'true'
+    }  
+    
+    this.updateColor = this.updateColor.bind(this);
+    this.updateSize = this.updateSize.bind(this);
+    this.updateFamily = this.updateFamily.bind(this);
+    this.updateEditStatus = this.updateEditStatus.bind(this);
+  }
 
-  // updateColor
+  updateColor(val){
+    this.setState({
+      fontColor: val
+    })
+  }
 
-  // updateSize
+  updateSize(val){
+    this.setState ({
+      fontSize: val
+    })
+  }
 
-  // updateFamily
+   updateFamily(val){
+    this.setState ({
+      fontFamily: val
+    })
+  }
 
-  // updateEditStatus
+   updateEditStatus(val){
+    this.setState ({
+      allowEdit: val
+    })
+  }
 
   render() {
     return (
